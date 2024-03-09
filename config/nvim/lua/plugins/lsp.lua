5e98495497c1d8a470a40d7d05c88a64c158311a
takesei <sei06k14@gmail.com>
2024-03-09 15:52:06 +0900

    :bug: :whale: :bee:

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,26 @@
+local mason_lspconfig = {
+  ensure_installed = { "lua_ls", "rust_analyzer", 'ruff_lsp', 'taplo', 'marksman' },
+  automatic_installation = true,
+  handlers = {
+    function(server)
+      require('lspconfig')[server].setup {
+        capabilities = require('cmp_nvim_lsp').default_capabilities()
+      }
+    end,
+
+    ["lua_ls"] = function()
+      require("lspconfig").lua_ls.setup {
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } }
+          }
+        }
+      }
+    end,
+  }
+}
+
+
 return {
   {
     "williamboman/mason.nvim",
@@ -22,13 +45,14 @@ return {
     lazy = true,
     event = 'BufRead',
     keys = {
-      { 'K',        function() vim.lsp.buf.hover() end,         'n' },
-      { 'gd',       function() vim.lsp.buf.definition() end,    'n' },
-      { 'gf',       function() vim.lsp.buf.format() end,        'n' },
-      { 'gr',       function() vim.lsp.buf.references() end,    'n' },
-      { 'gn',       function() vim.lsp.buf.rename() end,        'n' },
-      { 'ga',       function() vim.lsp.buf.code_action() end,   'n' },
-      { '[Space]`', function() vim.diagnostic.open_float() end, 'n' },
+      { 'K',  function() vim.lsp.buf.hover() end,        'n' },
+      { 'gd', function() vim.lsp.buf.definition() end,   'n' },
+      { 'gf', function() vim.lsp.buf.format() end,       'n' },
+      { 'gr', function() vim.lsp.buf.references() end,   'n' },
+      { 'gn', function() vim.lsp.buf.rename() end,       'n' },
+      { 'ga', function() vim.lsp.buf.code_action() end,  'n' },
+      { 'g[', function() vim.diagnostic.goto_next() end, 'n' },
+      { 'g]', function() vim.diagnostic.goto_prev() end, 'n' },
     },
     config = function()
       vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
@@ -37,27 +61,7 @@ return {
         end
       })
 
-      require("mason-lspconfig").setup {
-        ensure_installed = { "lua_ls", "rust_analyzer", 'ruff_lsp', 'taplo', 'marksman' },
-        automatic_installation = true,
-        handlers = {
-          function(server)
-            require('lspconfig')[server].setup {
-              capabilities = require('cmp_nvim_lsp').default_capabilities()
-            }
-          end,
-
-          ["lua_ls"] = function()
-            require("lspconfig").lua_ls.setup {
-              settings = {
-                Lua = {
-                  diagnostics = { globals = { "vim" } }
-                }
-              }
-            }
-          end,
-        }
-      }
+      require("mason-lspconfig").setup(mason_lspconfig)
     end
   }
 }
